79e500b5962da2cb152d99a78c76bb2ac0a74398
tasmina183826 <78465572+tasmina183826@users.noreply.github.com>
2022-01-30 14:10:25 +0630

    Add files via upload

---
+++ reg.php
@@ -0,0 +1,97 @@
+<html>
+<body>
+<h1>Registration form </h1>
+<form>
+<table>
+
+<tr>
+ <td> <label for="firstname">First name:</label><br></td>
+ <td> <input type="text" id="firstname" name="firstname"</td>
+</tr>
+<tr>
+ <td></td>
+</tr>
+<tr>
+ <td></td>
+</tr>
+<tr>
+ <td><label for="lastname">Last name:</label></td>
+ <td><input type="text" id="lastname" name="lastname"</td>
+</tr>
+
+<tr>
+ <td></td>
+</tr>
+<tr>
+ <td></td>
+</tr>
+<tr>
+ <td><label for="age">Age:</label></td>
+ <td> <input type="age" id="age" name="age"></td>
+</tr>
+
+<tr>
+ <td></td>
+</tr>
+<tr>
+ <td></td>
+</tr>
+  <td>Designation:</td>
+  <td><input type="radio" id="Programmer" name="Programmer" value="Junior Programmer">
+  <label for="Programmer">Junior Programmer</label>
+  <input type="radio" id="Programmer" name="Programmer" value="Senior Programmer">
+  <label for="Programmer">Senior Programmer</label>
+  <input type="radio" id="Programmer" name="Programmer" value="Project Lead">
+  <label for="Programmer">Project Lead.</label></td>
+</tr>
+<tr>
+   <td>Preferred Languague:</td>
+   <td>
+  <input type="checkbox" id="Languague1" name="Languague1" value="Java">
+  <label for="Languague1"> JAVA</label>
+  <input type="checkbox" id="Languague2" name="Languague2" value="PHP">
+  <label for="Languague2"> PHP</label>
+  <input type="checkbox" id="Languague3" name="Languague3" value="C++">
+  <label for="Languague3"> C++</label>
+  </td>
+  
+</tr>
+<tr>
+<tr>
+ <td><label for="email">Email:</label></td>
+ <td> <input type="email" id="email" name="email"></td>
+</tr>
+<tr>
+ <td></td>
+</tr>
+<tr>
+ <td></td>
+</tr>
+<tr>
+  <td> <label for="pwd">Password:</label></td>
+  <td><input type="pwd" id="pwd" name="pwd" minlength="8"></td>
+</tr>
+<tr>
+ <td></td>
+</tr>
+<tr>
+ <td></td>
+</tr>
+<tr>
+<td>
+  <label for="myfile">Please Choose a file:</label></td>
+  <td><input type="file" id="myfile" name="myfile"></td>
+  </td>
+  </tr>
+  <tr>
+  <td><p></p><br></Td>
+  </tr>
+  <tr>
+   
+  <td><input type="submit"> <input type="Reset"></td>
+  </tr>
+  </table>
+</form>
+
+</body>
+</html>
